ae0a8d6241548a89781203f6cb749025274750c8
mjnakaji <mjnakaji@ucsc.edu>
2021-07-01 02:27:14 -0700

    more comments added

--- src/main.js
+++ src/main.js
@@ -1,3 +1,28 @@
+/*
+
+Name: Masateru Nakajima
+Project Title: Modded Rocket Patrol
+Date: July 1st, 2021
+How long it took to complete the whole project: nearly a week including the tutorial
+
+Point Breakdown:
+    20 pts - Finishing the Tutorial
+    30 pts - Implement a simultaneous two-player mode
+    20 pts - Create and implement a new weapon (w/ new behavior and graphics)
+                I created a beam that triggers with a space bar that demolishes everything on screen
+    5 pts  - Allow the player to control the Rocket after it's fired
+    5 pts  - Add your own (copyright-free) background music to the Play scene
+
+    20 pts(?) - Create new title screen, but extra
+                    The user is able to alternate the title screen with opposite colors in the color wheel
+    10 pts(?) - Added even more sound effects in the case of impact and action
+    5 pts(?)  - Bug fix: Disabled rapid fire sound while in the shooting animation
+
+
+I am certain that all the work done here is my own, except for the professor's code shown in lecture.
+
+*/
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
